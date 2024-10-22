96e5061821100edd639ace09aae00e3f6d1f3b4e
Giuseppe <giuseppe.costantino95@gmail.com>
2024-10-22 17:10:23 +0200

    Update DenoisedDataHandler.py

--- denoised_data_utils/DenoisedDataHandler.py
+++ denoised_data_utils/DenoisedDataHandler.py
@@ -10,7 +10,7 @@ class DenoisedDataHandler:
 
     @property
     def corrected_time(self):
-        if self.corrected_time is None:
+        if self._corrected_time is None:
             time_array = self.ddl.get_time_array()
             corrected_time = time_array[0 + self.offset:-self.window_length - self.offset]
             self.corrected_time = corrected_time
